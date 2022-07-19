3ca5ac61928cb76159d60b77b37c1283e8a21dcd
azanniel <leo.azannielttt@gmail.com>
2022-07-18 20:14:05 -0400

    add signIn and hom screen

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+import { THEME } from './src/styles/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        translucent
+        barStyle='light-content'
+        backgroundColor='transparent'
+      />
+
+      {fontsLoaded ? <Home /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
