526c7ab0d6c63f9fb5f234cec93d0d742ac10333
azanniel <leo.azannielttt@gmail.com>
2022-07-20 11:38:49 -0400

    load orders from firestore

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import { HStack, VStack, IconButton, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { ChatTeardropText, SignOut } from 'phosphor-react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import { Loading } from '../components/Loading';
 import { Filter } from '../components/Filter';
 import { Order, OrderProps } from '../components/Order';
 import { Button } from '../components/Button';
 
 import Logo from '../assets/logo_secondary.svg';
+import { dateFormat } from '../utils/firestoreDateFormat';
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-  const [orders, setOrders] = useState<OrderProps[]>([{
-    id: '1234',
-    patrimony: '21313',
-    when: '18/07/2022 às 14:00',
-    status: 'open'
-  }]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
 
   const navigation = useNavigation();
   const { colors } = useTheme();
@@ -33,12 +32,38 @@ export function Home() {
 
   function handleLogOut() {
     auth()
-    .signOut()
-    .catch((error) => {
-      console.log(error);
-      return Alert.alert('Sair', 'Não foi possível sair.')
-    });
+      .signOut()
+      .catch((error) => {
+        console.log(error);
+        return Alert.alert('Sair', 'Não foi possível sair.')
+      });
   }
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const subscriber = firestore()
+      .collection('orders')
+      .where('status', '==', statusSelected)
+      .onSnapshot(snapshot => {
+        const data = snapshot.docs.map(doc => {
+          const { patrimony, description, status, created_at } = doc.data();
+
+          return {
+            id: doc.id,
+            patrimony,
+            description,
+            status,
+            when: dateFormat(created_at)
+          }
+        });
+
+        setOrders(data);
+        setIsLoading(false);
+      });
+
+    return subscriber;
+  }, [statusSelected])
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -86,31 +111,27 @@ export function Home() {
           />
         </HStack>
 
-        <FlatList
-          data={orders}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <Order 
-              data={item}
-              onPress={() => handleOpenDetails(item.id)}
+        {
+          isLoading ? <Loading /> :
+            <FlatList
+              data={orders}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={{ paddingBottom: 100 }}
+              ListEmptyComponent={() => (
+                <Center>
+                  <ChatTeardropText size={40} color={colors.gray[300]} />
+                  <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                    Você ainda não tem {'\n'}
+                    solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                  </Text>
+                </Center>
+              )}
             />
-          )}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{
-            paddingBottom: 100
-          }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText size={40} color={colors.gray[300]} />
-              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                Você ainda não tem {'\n'}
-                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-              </Text>
-            </Center>
-          )}
-        />
+        }
 
-        <Button 
+        <Button
           title='Nova solicitação'
           onPress={handleNewOrder}
         />
